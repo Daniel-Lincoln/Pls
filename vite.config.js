2cb8e24d400b6b8a07cb6bdc0fcdb04f984a1625
Daniel-Lincoln <78099817+Daniel-Lincoln@users.noreply.github.com>
2024-04-19 12:15:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['swiper']
+    }
+  }
   plugins: [
     vue(),
   ],
@@ -14,10 +19,3 @@ export default defineConfig({
     }
   }
 })
-export default {
-  build: {
-    rollupOptions: {
-      external: ['swiper']
-    }
-  }
-}
